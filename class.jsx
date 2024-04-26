53bea41051cc048a9b08ab022c0d05bfb8a97cf0
21PM <parasmore33@gmail.com>
2024-04-27 00:53:29 +0530

    commit

---
+++ class.jsx
@@ -0,0 +1,69 @@
+
+const Title = () =>{
+    return(
+        <h1>Learn Web Development</h1>
+    )
+}
+
+
+const Para1 = () =>{
+    return(
+       <div>
+         <p>Welcome to the MDN learning area. This set of articles aims to guide complete beginners to web development with all that they need to start coding websites.</p>
+       </div>
+    )
+}
+
+const Para2 = () =>{
+    return(
+        <div>
+            <p>The aim of this area of MDN is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from <a href="https://developer.mozilla.org/en-US/" target="_blank">the rest of MDN</a>, and other intermediate to advanced resources that assume a lot of previous knowledge.</p>
+        </div>
+    )
+}
+
+const Para3 = () =>{
+    return(
+        <p>If you are a complete beginner, web development can be challenging your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.</p>
+    )
+}
+
+
+const para = [`Welcome to the MDN learning area. This set of articles aims to guide complete beginners to web development with all that they need to start coding websites.
+`,
+`The aim of this area of MDN is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from <a href="https://developer.mozilla.org/en-US/" target="_blank">the rest of MDN</a>, and other intermediate to advanced resources that assume a lot of previous knowledge.`,
+
+`If you are a complete beginner, web development can be challenging your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.`]
+
+
+
+// const Paragraphs =() =>{
+
+   
+//     return(
+
+//         <div>
+//         {
+//             para.map((p)=>{
+//                 return <p>{p}</p>
+//             })
+            
+//         }
+//         </div>
+//     )
+// }
+
+const MainContainer = () =>{
+    return(
+        <div id="inner_main">
+            <div className="title">
+            <Title/>
+            </div>
+            <Para1/>
+            <Para2/>
+            <Para3/>
+        </div>
+    )
+}
+
+ReactDOM.render(<MainContainer/>,document.getElementById("main"));
---
+++ script.js
@@ -0,0 +1,7 @@
+const MyreactComp = () =>{
+    return (
+        <h1>Using React</h1>
+    );
+};
+
+ReactDOM.render(<MyreactComp/>,document.getElementById("main"));
